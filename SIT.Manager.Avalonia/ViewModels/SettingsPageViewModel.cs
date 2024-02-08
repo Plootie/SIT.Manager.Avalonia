6b862f920f40bb8193105c5493280186cf39f8ba
artehe <mflitcroft@pm.me>
2024-02-08 00:01:56 +0000

    Add manager version

--- SIT.Manager.Avalonia/ViewModels/SettingsPageViewModel.cs
+++ SIT.Manager.Avalonia/ViewModels/SettingsPageViewModel.cs
@@ -2,41 +2,46 @@
 using SIT.Manager.Avalonia.Models;
 using SIT.Manager.Avalonia.Services;
 using System.ComponentModel;
+using System.Reflection;
 
-namespace SIT.Manager.Avalonia.ViewModels
+namespace SIT.Manager.Avalonia.ViewModels;
+
+public partial class SettingsPageViewModel : ViewModelBase
 {
-    public partial class SettingsPageViewModel : ViewModelBase
-    {
-        private IManagerConfigService _configsService;
+    private readonly IManagerConfigService _configsService;
 
-        private bool _isLoaded = false;
+    private readonly bool _isLoaded = false;
 
-        [ObservableProperty]
-        private bool _closeAfterLaunch;
+    [ObservableProperty]
+    private bool _closeAfterLaunch;
 
-        [ObservableProperty]
-        private bool _lookForUpdates;
+    [ObservableProperty]
+    private bool _lookForUpdates;
 
-        public SettingsPageViewModel(IManagerConfigService configService) {
-            _configsService = configService;
+    [ObservableProperty]
+    private string _managerVersionString;
 
-            CloseAfterLaunch = _configsService.Config.CloseAfterLaunch;
-            LookForUpdates = _configsService.Config.LookForUpdates;
+    public SettingsPageViewModel(IManagerConfigService configService) {
+        _configsService = configService;
 
-            _isLoaded = true;
-        }
+        CloseAfterLaunch = _configsService.Config.CloseAfterLaunch;
+        LookForUpdates = _configsService.Config.LookForUpdates;
 
-        protected override void OnPropertyChanged(PropertyChangedEventArgs e) {
-            base.OnPropertyChanged(e);
+        ManagerVersionString = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "N/A";
 
-            if (_isLoaded) {
-                ManagerConfig config = _configsService.Config;
-                config.CloseAfterLaunch = CloseAfterLaunch;
-                config.LookForUpdates = LookForUpdates;
+        _isLoaded = true;
+    }
 
-                _configsService.UpdateConfig(config);
-                _configsService.Save();
-            }
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e) {
+        base.OnPropertyChanged(e);
+
+        if (_isLoaded) {
+            ManagerConfig config = _configsService.Config;
+            config.CloseAfterLaunch = CloseAfterLaunch;
+            config.LookForUpdates = LookForUpdates;
+
+            _configsService.UpdateConfig(config);
+            _configsService.Save();
         }
     }
 }
