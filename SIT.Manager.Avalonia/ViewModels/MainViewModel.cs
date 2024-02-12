f810693718e68b111c2cb090b6e5410864548e00
artehe <mflitcroft@pm.me>
2024-02-12 15:28:48 +0000

    implement notifications for the top bar of the main view

--- SIT.Manager.Avalonia/ViewModels/MainViewModel.cs
+++ SIT.Manager.Avalonia/ViewModels/MainViewModel.cs
@@ -1,5 +1,28 @@
-﻿namespace SIT.Manager.Avalonia.ViewModels;
+﻿using SIT.Manager.Avalonia.Models;
+using SIT.Manager.Avalonia.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace SIT.Manager.Avalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    private readonly IBarNotificationService _barNotificationService;
+
+    public ObservableCollection<BarNotification> BarNotifications { get; } = [];
+
+    public MainViewModel(IBarNotificationService barNotificationService) {
+        _barNotificationService = barNotificationService;
+
+        _barNotificationService.BarNotificationReceived += BarNotificationService_BarNotificationReceived;
+    }
+
+    private async void BarNotificationService_BarNotificationReceived(object? sender, BarNotification e) {
+        BarNotifications.Add(e);
+        if (e.Delay > 0) {
+            await Task.Delay(TimeSpan.FromSeconds(e.Delay));
+            BarNotifications.Remove(e);
+        }
+    }
 }
