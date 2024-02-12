14631860994908fbb61446c508cc9651fa0a8b8c
artehe <mflitcroft@pm.me>
2024-02-12 16:06:40 +0000

    set default to use empty string

--- SIT.Manager.Avalonia/Models/BarNotification.cs
+++ SIT.Manager.Avalonia/Models/BarNotification.cs
@@ -4,7 +4,7 @@ namespace SIT.Manager.Avalonia.Models
 {
     public class BarNotification
     {
-        public string Title { get; set; } = "Notiication";
+        public string Title { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
         public InfoBarSeverity Severity { get; set; } = InfoBarSeverity.Informational;
         public int Delay { get; set; } = 5;
