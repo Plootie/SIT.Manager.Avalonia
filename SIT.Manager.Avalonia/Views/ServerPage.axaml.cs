94203d3f1d2d5aa0f474caf6c23848a9c4eac9a4
artehe <mflitcroft@pm.me>
2024-02-16 09:08:12 +0000

    fix for scrolling on the server page

--- SIT.Manager.Avalonia/Views/ServerPage.axaml.cs
+++ SIT.Manager.Avalonia/Views/ServerPage.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using SIT.Manager.Avalonia.ViewModels;
@@ -6,6 +7,7 @@ namespace SIT.Manager.Avalonia.Views
 {
     public partial class ServerPage : UserControl
     {
+        private bool _autoScroll = true;
         private readonly ScrollViewer? _consoleLogScroller;
 
         public ServerPage() {
@@ -14,8 +16,28 @@ namespace SIT.Manager.Avalonia.Views
             _consoleLogScroller = this.FindControl<ScrollViewer>("ConsoleLogScroller");
         }
 
-        public void ConsoleLogItemsControl_SizeChanged(object? sender, SizeChangedEventArgs e) {
-            _consoleLogScroller?.ScrollToEnd();
+        private void ConsoleLogScroller_ScrollChanged(object? sender, ScrollChangedEventArgs e) {
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentDelta == Vector.Zero) {
+                // Content unchanged : user scroll event
+                if (_consoleLogScroller?.Offset.Y == _consoleLogScroller?.ScrollBarMaximum.Y) {
+                    // Scroll bar is in bottom
+                    // Set auto-scroll mode
+                    _autoScroll = true;
+                }
+                else {
+                    // Scroll bar isn't in bottom
+                    // Unset auto-scroll mode
+                    _autoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (_autoScroll && e.ExtentDelta != Vector.Zero) {
+                // Content changed and auto-scroll mode set
+                // Autoscroll
+                _consoleLogScroller?.ScrollToEnd();
+            }
         }
     }
 }
